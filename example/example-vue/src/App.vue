c61e6ead9785a45b34504ea75173799d7cfaa801
Kntt <kntt9968@gmail.com>
2019-02-17 14:33:34 +0800

    feat: example complete

--- example/example-vue/src/App.vue
+++ example/example-vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to use vue-js-bridge"/>
   </div>
 </template>
 
@@ -23,6 +23,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+}
+img {
+  width: 100px;
 }
 </style>
--- example/example-vue/src/main.js
+++ example/example-vue/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueJsBridge from 'vue-js-bridge'
 
+Vue.use(VueJsBridge, {
+  debug: true,
+  nativeHandlerName: 'testObjcCallback',
+  mock: false,
+  mockHandler (payload, next) {
+    next(Object.assign({ form: 'native' }, payload))
+  }
+})
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
